1c682303a40308e4423b25f330d017c282ac611d
jingjinge150 <jgh1106@naver.com>
2025-12-22 11:50:16 +0900

    지원_app.js 주석처리

--- QueChalPang/app.js
+++ QueChalPang/app.js
@@ -1,18 +1,18 @@
 // app.js
-const express = require("express");
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.listen(port, () => {
-  console.log("Server start");
-  console.log(`http://localhost:${port}`);
-});
-app.get("/", (req, res) => {
-  res.send("Welcome! Rest Server");
-});
-const boardRouter = require("./routers/boardRouter.js");
-const commentRouter = require("./routers/commentRouter.js");
-app.use("/api", boardRouter); // http://localhost:3000/api/boards
-app.use("/api", commentRouter); // http://localhost:3000/api/comments
+  console.log('Server start')
+  console.log(`http://localhost:${port}`)
+})
+app.get('/', (req, res) => {
+  res.send('Welcome! Rest Server')
+})
+// const boardRouter = require('./routers/boardRouter.js')
+// const commentRouter = require('./routers/commentRouter.js')
+// app.use('/api', boardRouter) // http://localhost:3000/api/boards
+// app.use('/api', commentRouter) // http://localhost:3000/api/comments
