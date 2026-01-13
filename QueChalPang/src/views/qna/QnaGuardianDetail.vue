e52f8d40b16a59360302bb234ef55fffe57f8f80
mocha2855 <mocha2855@gmail.com>
2026-01-13 20:49:19 +0900

    0113 다정 어제 푸쉬 안한거

--- QueChalPang/src/views/qna/QnaGuardianDetail.vue
+++ QueChalPang/src/views/qna/QnaGuardianDetail.vue
@@ -23,6 +23,9 @@ onMounted(fetchDetail)
 
 <template>
   <div class="qna-detail" v-if="qna">
+    <div>
+      <button class="back-btn btn-warning btn-sm fs-6" @click="$router.back()">← 뒤로가기</button>
+    </div>
     <div class="qna-card">
       <div class="qna-title">
         {{ qna.qna_title }}
